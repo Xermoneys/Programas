d4bc4ce881e7bf53e41095a197f801c38c8c2fa6
Xermoneys <102992203+Xermoneys@users.noreply.github.com>
2022-07-06 14:31:42 -0300

    Código pronto(EP0)

--- ex/EP0.c
+++ ex/EP0.c
@@ -1,30 +1,84 @@
 #include <stdio.h>
 
+int calculaT(int valorIn);
+
+void menu();
+
 int main(void)
 {
-    int N, print = 0;
-    do
+    int i;
+    int N, opcao;
+    char lucas = 'd';
+
+    printf("||||||||EP0||||||||\n");
+    while (1)
     {
-        printf("Digite um número inteiro maior ou igual a 0\n");
-        scanf("%i", &N);
-    } while (N < 0);
-    if (N <= 2)
-        print = 0;
-    else if(N == 3)
-        print = 1;
+        menu();
+        scanf("%i", &opcao);
+        switch (opcao)
+        {
+        case 1:
+            do
+            {
+                printf("Digite um número inteiro maior ou igual a 0\n");
+                scanf("%i", &N);
+            } while (N < 0);
+            printf("T%i = %i\n", N, calculaT(N));
+            break;
+        case 2:
+            do
+            {
+                printf("Escolha o tamanho da sequencia: ");
+                scanf("%i", &N);
+            } while (N < 0);
+            for (i = 0; i < N; i++)
+            {
+                // if(i != 0)// 0|, 246|, 515|, 264
+                //    printf(", ");
+                printf("T%i = %i\n", i, calculaT(i)); 
+                // if(i < N-1) // 0, |246, |515, |264
+                //    printf(", ");
+            }
+            printf("\n");
+            break;
+        case 3:
+            return 0;
+            break;
+        default:
+            break;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+void menu()
+{
+    printf("  1) Escolha o TN\n");
+    printf("  2) Escolha o tamanho da sequencia\n");
+    printf("  3) Sair\n");
+    printf("Digite a opcao desejada: ");
+}
+
+int calculaT(int valorIn)
+{
+    int i = 0;
+    int a = 0, b = 0, c = 0, d = 1;
+    int print = 0;
+    if (valorIn <= 2)
+        return 0;
+    else if (valorIn == 3)
+        return 1;
     else
     {
-        for (N = 4; N >= 4; N++)
+        for (i = 4; i <= valorIn; i++)
         {
-            N = N-1 + N-2 + N-3 + N-4;
-            printf("N = %i", N);
+            print = d + c + b + a;
+            a = b;
+            b = c;
+            c = d;
+            d = print;
         }
+        return print;
     }
-    printf("T%i = %i\n", N, print);
-    return 0;
-    for (size_t i = 0; i < count; i++)
-    {
-        /* code */
-    }
-    
-} 
+}
