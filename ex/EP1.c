57c569793756c2350a9d36b685f95e90d14a35b3
Xermoneys <102992203+Xermoneys@users.noreply.github.com>
2022-07-07 15:20:46 -0300

    menu, calculator, ints

---
+++ ex/EP1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int calculaT(int valorIn)
+
+void menu()
+
+int main(void)
+{
+    int i;
+    int N, opcao;
+
+    printf ("EP1: Soma de quadrados de primos consecutivos\n")
+    while(1)
+    {
+        menu();
+        scanf("%i", &opcao);
+        switch (opcao)
+        {
+        case 1:
+            do
+            {
+                printf("Digite um número (0 < n < 10^8)\n")
+                scanf("%i", &N);
+            } while (N < 0);
+            break;
+        case 2:
+        do
+        {
+           printf("")
+        } while (/* condition */);
+        
+
+            break;
+        }
+    }
+
+void menu()
+    {
+        printf("  1) Escolha o número para a sequência\n");
+        printf("  2) Sair\n");
+        printf("Digite a opção desejada: ");
+    }
+
+int calculaT(int valorIn)
+{
+    int i 
+
+}
+}
